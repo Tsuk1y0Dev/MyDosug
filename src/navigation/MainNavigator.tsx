65028e7a6ee9cb40079185bcd3936c755d8c5d5d
Tsuk1y0Dev <tsuk1y0develope@gmail.com>
2025-10-27 11:51:43 +0900

    Fixed MainNavigator

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,30 +1,22 @@
-// src/navigation/MainNavigator.tsx
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/main/HomeScreen';
+import { SearchScreen } from '../screens/main/SearchScreen';
 import { PlannerScreen } from '../screens/main/PlannerScreen';
-// ... другие импорты
+import { RoutesScreen } from '../screens/main/RoutesScreen';
+import { ProfileScreen } from '../screens/main/ProfileScreen';
+import { MainTabParamList } from './types';
 
-const Tab = createBottomTabNavigator();
-const PlannerStack = createStackNavigator();
-
-const PlannerStackNavigator = () => (
-  <PlannerStack.Navigator screenOptions={{ headerShown: false }}>
-    <PlannerStack.Screen name="PlannerMain" component={PlannerScreen} />
-  </PlannerStack.Navigator>
-);
+const Tab = createBottomTabNavigator<MainTabParamList>();
 
 export const MainNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen 
-        name="Planner" 
-        component={PlannerStackNavigator}
-        options={{ tabBarButton: () => null }} // Скрыть из таббара
-      />
-      {/* ... другие табы */}
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Planner" component={PlannerScreen} />
+      <Tab.Screen name="Routes" component={RoutesScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 };
